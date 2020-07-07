854085e8ef49aaad06fc0954ea7521075c7fb734
hungry-eater <didwogns789@naver.com>
2020-07-07 10:16:23 +0900

    Core Middleware install helmet, morgan, cookie-parser, body-parser

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 import express from "express"
+import helmet from "helmet"
+import morgan from "morgan"
+import cookieParser from "cookie-parser"
+import bodyParser from "body-parser"
+
 const app = express()
+
+app.use(helmet())
+app.use(morgan("dev"))
+app.use(cookieParser())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
 
 const handleHome = (req, res) => {
     res.send("Home")
