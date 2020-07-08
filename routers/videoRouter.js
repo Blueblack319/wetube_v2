aae84bbbad516563b1301e62fd75b460818e9a0a
hungry-eater <didwogns789@naver.com>
2020-07-08 11:28:08 +0900

    Clear

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -7,6 +7,7 @@ import {
   videos,
   deleteVideo,
   getUpload,
+  getEditVideo,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
@@ -16,7 +17,9 @@ videoRouter.get(routes.home, videos);
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, getUpload);
 
-videoRouter.get(routes.editVideo, editVideo);
+videoRouter.get(routes.editVideo, getEditVideo);
+// videoRouter.post(routes.editProfile)
+
 videoRouter.get(routes.deleteVideo, deleteVideo);
 videoRouter.get(routes.videoDetail, videoDetail);
 
